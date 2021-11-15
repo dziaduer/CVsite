021baf00c89b061f38916251967107126a1e0302
dziaduer <dziaduer@gmail.com>
2021-11-15 18:56:12 +0100

    Added the coolest function yet FLAG TAG Also icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-!function copyToClipboard(element) {
+function copyToClipboard(id) {
     /* Get the text field */
-    var copyText = document.getElementById(element);
-  
-    /* Select the text field */
+    var copyText = document.getElementById(id);
+    
+
     copyText.select();
+    
+    
     copyText.setSelectionRange(0, 99999); /* For mobile devices */
   
      /* Copy the text inside the text field */
@@ -12,3 +14,6 @@
     /* Alert the copied text */
     alert("Copied the text: " + copyText.value);
   } 
+  function myFunction(id) {
+    document.getElementById(id).innerHTML = "Paragraph changed.";
+  }
